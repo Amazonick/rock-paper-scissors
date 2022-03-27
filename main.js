f365152a0e7232a286c45b2ba98fcc7ed295eec0
Amazonick <pzs@live.com>
2022-03-27 17:34:41 +0200

    Added UI

--- main.js
+++ main.js
@@ -1,89 +1,88 @@
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const playerChoiceDisplay = document.getElementById('player-choice');
+const resultDisplay = document.getElementById('result');
+const scoreDisplay = document.getElementById('score-board');
+const possibleChoices = document.querySelectorAll('button');
+let playerChoice;
+let computerChoice;
+let result;
+
 // score board
 let playerScore = 0;
 let computerScore = 0;
 let gamesPlayed = 0;
 
-// Player Selection
-let playerSelection
-
-
-
+possibleChoices.forEach((button) =>
+  button.addEventListener('click', (e) => {
+    playerChoice = e.target.id;
+    playerChoiceDisplay.innerHTML = playerChoice;
+    computerChoiceDisplay.innerHTML = computerRandomChoice();
+    game();
+  })
+  );
 
 // Computer Selection
-let computerSelection = computerSelect();
-function computerSelect() {
-    const computerPick = ['rock', 'paper', 'scissors'];
-    const computerRandom = Math.floor(Math.random()*3);
-    return computerPick[computerRandom]; 
-};
+function computerRandomChoice() {
+  const computerPick = ['rock', 'paper', 'scissors'];
+  const computerRandom = Math.floor(Math.random() * 3);
+  return (computerChoice = computerPick[computerRandom]);
+}
 
 // Play Round
-function playRound(playerSelection, computerSelection) {
-
-
-        if (playerSelection === "rock" && computerSelection === "rock") {
-                console.log(playerSelection + " & " + computerSelection + " is a tie!")
-        }   
-        else if (playerSelection === "rock" && computerSelection === "paper") {
-                console.log("You lose! Your choice: " + playerSelection + " Computer: " + computerSelection); 
-                computerScore++         
-        }   
-        else if (playerSelection === "rock" && computerSelection === "scissors") {
-                console.log("You win! Your choice: " + playerSelection + " Computer: " + computerSelection);
-                playerScore++
-        }
-        else if (playerSelection === "paper" && computerSelection === "paper") {
-                console.log(playerSelection + " & " + computerSelection + " is a tie!")
-        }   
-        else if (playerSelection === "paper" && computerSelection === "scissors") {
-                console.log("You lose! Your choice: " + playerSelection + " Computer: " + computerSelection); 
-                computerScore++ 
-        }   
-        else if (playerSelection === "paper" && computerSelection === "rock") {
-                console.log("You win! Your choice: " + playerSelection + " Computer: " + computerSelection);
-                playerScore++
-        }
-        else if (playerSelection === "scissors" && computerSelection === "scissors") {
-                console.log(playerSelection + " & " + computerSelection + " is a tie!")
-        }   
-        else if (playerSelection === "scissors" && computerSelection === "rock") {
-                console.log("You lose! Your choice: " + playerSelection + " Computer: " + computerSelection); 
-                computerScore++ 
-        }   
-        else {
-                console.log("You win! Your choice: " + playerSelection + " Computer: " + computerSelection);
-                playerScore++
-        }
-        
+function playRound() {
+  // Player: ROCK
+  if (playerChoice === 'rock' && computerChoice === 'rock') {
+    result = "It's a tie!";
+  } else if (playerChoice === 'rock' && computerChoice === 'paper') {
+    result = 'You lose!';
+    computerScore++;
+  } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
+    result = 'You win!';
+    playerScore++;
+  }
+  // Player: PAPER
+  else if (playerChoice === 'paper' && computerChoice === 'paper') {
+    result = "It's a tie!";
+  } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
+    result = 'You lose!';
+    computerScore++;
+  } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+    result = 'You win!';
+    playerScore++;
+  }
+  // Player: SCISSORS
+  else if (playerChoice === 'scissors' && computerChoice === 'scissors') {
+    result = "It's a tie!";
+  } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
+    result = 'You lose!';
+    computerScore++;
+  } else {
+    result = 'You win!';
+    playerScore++;
+  }
+  console.log('Player score:' + playerScore);
+  console.log('Computer score:' + computerScore);
+  resultDisplay.innerHTML = result;
+  scoreDisplay.innerHTML = `Player: ${playerScore} | Computer: ${computerScore}`;
 }
 
 // multiple rounds
 function game() {
-        // player input
-        function playerSelect() {
-                return prompt("Choose your tool!").toLowerCase();
-        };
+  while (playerScore < 5 && computerScore < 5) {
+    playRound(playerChoice, computerChoice);
+    gamesPlayed++;
+    console.log('Player:' + playerScore, ' Comp:' + computerScore);
+    console.log('Rounds:' + gamesPlayed);
+    break;
+  }
 
-        while (playerScore < 5 && computerScore < 5) {
-            playRound(playerSelect(), computerSelect());
-            gamesPlayed++
-            console.log('Player:' + playerScore, ' Comp:' + computerScore);
-            console.log("Rounds:" + gamesPlayed)
-        }
-        
-        if (playerScore === 5) {
-                console.log("You won the game!");
-        } else if (computerScore === 5) {
-                console.log("You lost the game!");
-        } else if (playerScore === computerScore) {
-                console.log("It's a draw!");
-        }
-    } ;
-game()
-
+  if (playerScore === 5) {
+    resultDisplay.innerHTML = 'You won the game!';
+  } else if (computerScore === 5) {
+    resultDisplay.innerHTML = 'You lost the game!';
+  } else if (playerScore === 5 && computerScore === 5) {
+    resultDisplay.innerHTML = 'The game is a tie! No winners. :)'
+  }
+}
 // score
-console.log("Player score:" + playerScore)
-console.log("Computer score:" + computerScore)
-console.log("Rounds:" + gamesPlayed)
-
-
+console.log('Rounds:' + gamesPlayed);
